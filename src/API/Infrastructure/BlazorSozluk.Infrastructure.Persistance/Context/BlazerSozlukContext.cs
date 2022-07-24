da6a464bbe0ae94c4b6b0fd91917149d422512bb
Ahmet Cemal Sert <acemalsert@gmail.com>
2022-07-24 15:04:17 +0300

    conn str implemented

--- src/API/Infrastructure/BlazorSozluk.Infrastructure.Persistance/Context/BlazerSozlukContext.cs
+++ src/API/Infrastructure/BlazorSozluk.Infrastructure.Persistance/Context/BlazerSozlukContext.cs
@@ -1,11 +1,7 @@
 ﻿using BlazorSozluk.Api.Domain.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace BlazorSozluk.Infrastructure.Persistance.Context
 {
@@ -13,6 +9,10 @@ namespace BlazorSozluk.Infrastructure.Persistance.Context
     {
         public const string DEFAULT_SCHEMA = "dbo";
 
+        public BlazerSozlukContext()
+        {
+
+        }
         public BlazerSozlukContext(DbContextOptions options) : base(options)
         {
 
@@ -26,6 +26,18 @@ namespace BlazorSozluk.Infrastructure.Persistance.Context
         public DbSet<EntryCommentFavorite> EntryCommentFavorites { get; set; }
         public DbSet<EntryVote> EnrtyVotes { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connStr = "Server=localhost;Database=master;Trusted_Connection=True;";
+                optionsBuilder.UseSqlServer(connStr, opt =>
+                {
+                    opt.EnableRetryOnFailure();
+                });
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             OnBeforeSave();
---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistance/Extensions/Registiration.cs
@@ -0,0 +1,28 @@
+﻿using BlazorSozluk.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistance.Extensions
+{
+    public static class Registiration
+    {
+        public static IServiceCollection AddInfrastructureRegistiration(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<BlazerSozlukContext>(conf =>
+            {
+                var connStr = configuration["BlazorSozlukDbConnectionString"].ToString();
+                conf.UseSqlServer(connStr, opt =>
+                {
+                    opt.EnableRetryOnFailure();
+                });
+            });
+            return services;
+        }
+    }
+}
